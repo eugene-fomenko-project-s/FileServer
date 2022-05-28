1d586ab05646d8ee4ff5c2948b764c1b90ea49ce
Eugenetucha <50362636+Eugenetucha@users.noreply.github.com>
2022-05-29 02:51:45 +0700

    Update Main.java

--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -1,9 +1,25 @@
 package server;
 
 
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
+    static ExecutorService executeIt = Executors.newFixedThreadPool(2);
+
     public static void main(String[] args) {
-        new server();
+
+        try (ServerSocket server = new ServerSocket(12123, 50, InetAddress.getByName("127.0.0.1"))) {
+            Socket client = server.accept();
+            executeIt.execute(new server(client));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
